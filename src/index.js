b9b6710b85e8dec0518d8f2ee813f6ae36a943c7
Ismail Karafakioglu <ismailkarafakioglu@gmail.com>
2026-08-26 23:19:24 +0200

    add functions to manipulate projects

--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
 import "./style.css";
 
-console.log("Test")
+const projectsArray = []
+
+class Project {
+    constructor(title, description){
+        this.title = title;
+        this.description = description;
+        this.id = createRandomId()
+        this.todos = [];
+    }
+}
+
+class Todo {
+    constructor(title, description, dueDate, status, priority, notes){
+        this.title = title;
+        this.description = description;
+        this.id = createRandomId()
+        this.dueDate = dueDate;
+        this.status = status;
+        this.priority = priority;
+        this.notes = notes;
+    }
+}
+
+function createRandomId(){
+    return self.crypto.randomUUID()
+}
+
+export function addProject(title, description){
+    projectsArray.push(new Project(title, description))
+}
+
+function deleteProject(id){
+    const index = projectsArray.findIndex((element) => element.id === id)
+    projectsArray.splice(index,1)
+}
+
+export function displayProject(){
+    console.table(projectsArray)
+    projectsArray.forEach(element => {
+        console.log(element.todos)
+    });
+}
+
+//testing purposes to use them in console.
+window.addProject = addProject;
+window.deleteProject = deleteProject;
+window.displayProject = displayProject;
+
+
+
+const defaultTodo = new Todo("Default todo", "default desc", null, null, null)
+const defaultProject = new Project("Default Project", "This is a default project");
+
+
+projectsArray.push(defaultProject);
+defaultProject.todos.push(defaultTodo)
+
+const tempTodo = new Todo("x", null, null, null, null, null, null)
+defaultProject.todos.push(tempTodo)
+
+console.log(projectsArray)
+
