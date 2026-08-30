eb47be0f8eb763f24c6f5c295eab232ff9bb45b6
Ismail Karafakioglu <ismailkarafakioglu@gmail.com>
2026-08-31 01:03:41 +0200

    init todoDomHandler.js

---
+++ src/todoDomHandler.js
@@ -0,0 +1,4 @@
+import { projectsArray } from "./data.js";
+import { createElementTemplate } from "./helperFunctions.js";
+import { showModal, closeModal } from "./modalHandler.js";
+import { body, createProjectBtn, projectsDiv } from "./layout.js";
